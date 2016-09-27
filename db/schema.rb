724c94c36621f1e447715b2d4ff933a7dcee9de4
rebekahheacock <rebekah.heacock@gmail.com>
2016-09-27 13:01:39 -0400

    Initialize project
    Run bundle install
    Create and migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150804215003) do
+ActiveRecord::Schema.define(version: 20160811195653) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 20150804215003) do
   end
 
   create_table "comments", force: :cascade do |t|
-    t.text     "content"
+    t.string   "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
