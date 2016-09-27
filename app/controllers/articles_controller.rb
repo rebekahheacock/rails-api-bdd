f8aec26ce50b2e7619f364e2e1a3b15bd10a9e86
rebekahheacock <rebekah.heacock@gmail.com>
2016-09-27 16:06:16 -0400

    Refactor articles controller
    Add set_article method that runs before show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,17 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show]
   def index
     @articles = Article.all
     render json: @articles
   end
 
   def show
-    @article = Article.find(params[:id])
     render json: @article
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 end
